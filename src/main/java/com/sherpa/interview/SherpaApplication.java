0d4f6a91d4ab08d651e0272bcc0762b840b665e3
찰흙고양이 <claaycaat@gmail.com>
2024-06-23 16:37:07 +0900

    setup mongodb

---
+++ src/main/java/com/sherpa/interview/SherpaApplication.java
@@ -0,0 +1,1 @@
+package com.sherpa.interview;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class SherpaApplication {	public static void main(String[] args) {		SpringApplication.run(SherpaApplication.class, args);	}}
